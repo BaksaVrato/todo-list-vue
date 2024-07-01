83462c5c692d7e63e6a7617b42ee6e3fea7d02f9
BaksaVrato <vratusbaksa@gmail.com>
2024-07-01 14:04:38 +0200

    icons installed - fontawesome

---
+++ src/components/SiteNavbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <nav>
+      <router-link :to="{ name: 'home' }">Todo List App</router-link>
+    </nav>
+</header>
+</template>
+
+
+<script>
+export default {
+
+}
+</script>
